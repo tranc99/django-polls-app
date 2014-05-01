78235535b56ef93f9beae7a272d769199208c464
tmutunhire <tmutunhire@cmtnyc.com>
2014-05-01 16:51:22 -0400

    use generic views

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http importHttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.template import RequestContext
 from polls.models import Choice, Poll
 from django.http import Http404
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
-
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p},
-    						   context_instance =RequestContext(request))
-
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
-
-def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
@@ -33,7 +20,7 @@ def vote(request, poll_id):
 		selected_choice.votes += 1
 		selected_choice.save()
 
-		return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+		return HttpResponseRedirect(reverse('polls_results', args=(p.id,)))
 
 
 def results(request, poll_id):
